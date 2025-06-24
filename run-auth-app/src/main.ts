9cf29a55f40e80082eb11715cb7cdd9f2790236b
huken <huken@kedacom.com>
2025-06-24 17:37:49 +0800

    完成docker部署3008端口。

--- run-auth-app/src/main.ts
+++ run-auth-app/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port=3001
-  await app.listen(port);
-  console.log("Auth-service running on port ",port)
-  console.log("Auth-service running on port ",port)
-  console.log("Auth-service running on port ",port)
+  await app.listen(port,"0.0.0.0");
+  console.log("Auth-service 0.0.0.0running on port ",port)
+  console.log("Auth-service 0.0.0.0running on port ",port)
+  console.log("Auth-service 0.0.0.0running on port ",port)
 }
 bootstrap();
